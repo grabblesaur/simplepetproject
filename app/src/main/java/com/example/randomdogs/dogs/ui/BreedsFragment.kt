ab2f591f0495ea463cc62829a4501450be84e632
bbogdanov <bogdanov.bayar@gmail.com>
2023-09-24 16:13:39 +0300

    add viewbinding simplest usage in fragment

--- app/src/main/java/com/example/randomdogs/dogs/ui/BreedsFragment.kt
+++ app/src/main/java/com/example/randomdogs/dogs/ui/BreedsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.randomdogs.dogs.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.randomdogs.R
+import com.example.randomdogs.databinding.FragmentBreedsBinding
 import com.example.randomdogs.dogs.data.Breed
 import com.example.randomdogs.dogs.data.BreedRemoteDataSourceImpl
 import com.example.randomdogs.dogs.data.BreedRepositoryImpl
@@ -12,9 +14,12 @@ import com.example.randomdogs.dogs.domain.GetBreedListUseCase
 import com.example.randomdogs.dogs.presentation.BreedsViewModel
 import com.example.randomdogs.dogs.presentation.BreedsViewModelFactory
 
-class BreedsFragment : Fragment(R.layout.fragment_breeds) {
+class BreedsFragment : Fragment() {
 
-	lateinit var viewModel: BreedsViewModel
+	private lateinit var viewModel: BreedsViewModel
+
+	private var _binding: FragmentBreedsBinding? = null
+	private val binding get() = _binding!!
 
 	companion object {
 
@@ -22,11 +27,22 @@ class BreedsFragment : Fragment(R.layout.fragment_breeds) {
 			BreedsFragment()
 	}
 
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentBreedsBinding.inflate(inflater, container, false)
+		return binding.root
+	}
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
 		setupViewModel()
 		observeViewModel()
+
+		viewModel.loadDogs()
 	}
 
 	private fun setupViewModel() {
@@ -46,6 +62,12 @@ class BreedsFragment : Fragment(R.layout.fragment_breeds) {
 	}
 
 	private fun onBreedChanged(breeds: List<Breed>) {
+		binding.firstBreed.text = breeds.first().name
+		binding.secondBreed.text = breeds[1].name
+	}
 
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
 	}
 }

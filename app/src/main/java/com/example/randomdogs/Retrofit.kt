1c39cab8c80ca7a2ae445fd9cc16844017ce882f
bbogdanov <bogdanov.bayar@gmail.com>
2023-10-16 17:35:15 +0300

    move api key header to retrofit creation

--- app/src/main/java/com/example/randomdogs/Retrofit.kt
+++ app/src/main/java/com/example/randomdogs/Retrofit.kt
@@ -1,14 +1,31 @@
 package com.example.randomdogs
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitManager {
 	private const val BASE_URL = "https://api.thedogapi.com/v1/"
-	const val API_KEY = "live_NPhqzxfj0z2ve9f194RmKjJYKTKKMIjOGgpoujCYKjDH5R7cPavWrPLo8zfNSUlU"
+	private const val API_KEY = "live_NPhqzxfj0z2ve9f194RmKjJYKTKKMIjOGgpoujCYKjDH5R7cPavWrPLo8zfNSUlU"
+
+	private fun getInterceptor(): Interceptor =
+		Interceptor { chain ->
+			val request = chain.request().newBuilder()
+				.addHeader("x-api-key", API_KEY)
+				.build()
+
+			chain.proceed(request)
+		}
+
+	private fun getHttpClient(): OkHttpClient =
+		OkHttpClient.Builder()
+			.addInterceptor(getInterceptor())
+			.build()
 
 	val instance: Retrofit = Retrofit.Builder()
 		.baseUrl(BASE_URL)
+		.client(getHttpClient())
 		.addConverterFactory(GsonConverterFactory.create())
 		.build()
 }
--- app/src/main/java/com/example/randomdogs/dogs/api/ImageApi.kt
+++ app/src/main/java/com/example/randomdogs/dogs/api/ImageApi.kt
@@ -1,14 +1,11 @@
 package com.example.randomdogs.dogs.api
 
-import com.example.randomdogs.RetrofitManager
 import com.example.randomdogs.dogs.data.Image
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface ImageApi {
 
-	@Headers("x-api-key: ${RetrofitManager.API_KEY}")
 	@GET("images/search")
 	suspend fun get(
 		@Query("limit") limit: Int = 9,
